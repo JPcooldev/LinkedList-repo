8544896d91dcefdfadd66e9c9ce24c2f39acf98f
JPcooldev <100166799+JPcooldev@users.noreply.github.com>
2022-03-06 14:36:08 +0100

    Add files via upload

---
+++ Linked list.cpp
@@ -0,0 +1,14 @@
+//
+//  Linked list.cpp
+//  LinkedListJP
+//
+//  Created by Jakub Prokop on 05.02.2022.
+//
+#include <iostream>
+#include "Linked list.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LinkedListJP
+//
+//  Created by Jakub Prokop on 05.02.2022.
+//
+
+#include <iostream>
+#include <string>
+
+int main() {
+    
+    class Node {
+        std::string
+    }
+    
+    return 0;
+}
